9db43f271a71a19f95cc874ce76dc5e55d8ade58
Scott Danzig <2577300+sdanzig@users.noreply.github.com>
2023-10-05 13:37:23 -0400

    Initial User Upload server commit

---
+++ src/main/java/com/scottdanzig/useruploadserver/UserUploadServerApplication.java
@@ -0,0 +1,13 @@
+package com.scottdanzig.useruploadserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserUploadServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserUploadServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scottdanzig/useruploadserver/UserUploadServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scottdanzig.useruploadserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserUploadServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
